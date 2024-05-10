0355abf3415232a23be1eacad361af6cb71d30c7
Xiangying Sun <xiangyingsun@Xiangyings-MacBook-Air.local>
2024-05-10 11:35:15 -0700

    test

--- assignment1/helloworld.c
+++ assignment1/helloworld.c
@@ -4,7 +4,7 @@
 // email: jeo.lee@northeastern.edu
 
 int main() {
-  printf("Hello World.  This is [YourName].\n");
+  printf("ello World.  This is [YourName].\n");
   printf("I am assignment 1 for [This Semester].\n");
   return 0;
 }
