00fa62fa96ca2135d77b39f48a5b6ad128711fd2
xiangyingsun <xysun325@gmail.com>
2024-07-25 13:31:11 -0700

    submit hw11

--- assignment11/hw11b.c
+++ assignment11/hw11b.c
@@ -7,7 +7,17 @@ int d[20];
 
 long long int dp(int n) {
 
-    // Add your code here
+    if (n >= 20) {
+        printf("Value of n exceeds the limit (n < 20).\n");
+        return 0;
+    }
+
+    if (n==1) return 1;
+    if (n==2) return 2;
+    if (d[n] != 0) return d[n];
+
+    d[n] = dp(n-1) + dp(n-2) * 2;
+    return d[n];
 
 }
 
--- assignment11/hw11d.c
+++ assignment11/hw11d.c
@@ -7,7 +7,17 @@ int d[20];
 
 long long int dp(int n) {
 
-    // Add your code here
+    if (n >= 20) {
+        printf("Value of n exceeds the limit (n < 20).\n");
+        return 0;
+    }
+
+    if (n == 1) return 2;
+    if (n == 2) return 7;
+    if (d[n] != 0) return d[n];
+
+    d[n] = dp(n-1)*2 + dp(n-2)*3;
+    return d[n];
 
 }
 
